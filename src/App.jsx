0d4dceab4708a01b5ebc48e9f2ca1ec43d7450c4
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-29 09:40:30 +0600

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to chatmate</h1>
+      <Routes>
+<Route path="/" element={<Chat/>}/>
+<Route path="/login" element={<Login />}/>
+<Route path="/register" element={<Register />}/>
+<Route path="*" element={<Navigate to="/" />}/>
+      </Routes>
     </div>
   );
 }
